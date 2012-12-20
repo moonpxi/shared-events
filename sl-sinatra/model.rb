603de0fb33e247f4da43adaceede53797b2da314
moonpxi <paulo.schneider@gmail.com>
2012-12-20 11:32:56 -0200

    Properly creating events linked to the right users

--- sl-sinatra/model.rb
+++ sl-sinatra/model.rb
@@ -12,31 +12,31 @@ def bootstrap_model
   Event.remove
 
   unless User.find_by_name('Paulo')
-    %w{Paulo Cris}.each { |name| User.create(:name => name) }
+    users = %w{Paulo Cris}.map { |name| User.create(:name => name) }
 
 
     Event.create(:title => 'Something tomorrow',
                  :start_at => rand_today + 1,
-                 :participants => [User.first, User.last])
+                 :participants => [users.first, users.last])
 
     Event.create(:title => 'Something yesterday',
                  :start_at => rand_today - 1,
-                 :participants => [User.first])
+                 :participants => [users.first])
 
     Event.create(:title => 'Something today',
                  :description => 'Today is today is today',
                  :start_at => rand_today,
-                 :participants => [User.first])
+                 :participants => [users.first])
 
     Event.create(:title => 'Something next week',
                  :start_at => rand_today + 7,
-                 :participants => [User.first])
+                 :participants => [users.first])
 
 
     Event.create(:title => 'Something with start and end',
                  :start_at => DateTime.now,
                  :end_at => DateTime.now + 0.2,
-                 :participants => [User.first])
+                 :participants => [users.first])
 
    end
 end
