a468189d33ccf18ef2d4d331c9aaeecaf58626b1
moonpxi <paulo.schneider@gmail.com>
2013-01-19 18:04:08 +0000

    Setting up ring, compojure and hiccup dependencies

--- se-in-clj/project.clj
+++ se-in-clj/project.clj
@@ -1,3 +1,6 @@
 (defproject se-in-clj "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.2"]]
+  :plugins [[lein-ring "0.8.0"]])
