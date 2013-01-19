4994957c428f88d38934e65de1d6f7ef697bc952
moonpxi <paulo.schneider@gmail.com>
2013-01-19 18:08:48 +0000

    Very basic example of a webapp

--- se-in-clj/src/se_in_clj/core.clj
+++ se-in-clj/src/se_in_clj/core.clj
@@ -1,1 +1,15 @@
-(ns se-in-clj.core)
+(ns se-in-clj.core
+  (:use compojure.core
+        [hiccup.middleware :only (wrap-base-url)])
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            [compojure.response :as response]))
+
+(defroutes main-routes
+  (GET "/" [] "Main page")
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+(def app
+  (-> (handler/site main-routes)
+      (wrap-base-url)))
